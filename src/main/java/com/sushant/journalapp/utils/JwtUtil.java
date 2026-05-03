c0e40d4dd8978b15fc80bca01ebc9d6067dc4523
Sushant Hande <sushanthande1@gmail.com>
2026-05-03 12:33:55 +0530

    Increase JWT expiration time from 5 to 30 minutes

--- src/main/java/com/sushant/journalapp/utils/JwtUtil.java
+++ src/main/java/com/sushant/journalapp/utils/JwtUtil.java
@@ -28,7 +28,7 @@ public class JwtUtil {
                 .header().empty().add("typ", "JWT")
                 .and()
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 5)) // 5 minutes
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30)) // 30 minutes
                 .signWith(getSigningKey())
                 .compact();
     }
